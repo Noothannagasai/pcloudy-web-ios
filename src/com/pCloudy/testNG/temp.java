2eb568277ada592f532a49ddcfcd399ee3169381
Noothannagasai <98968250+Noothannagasai@users.noreply.github.com>
2024-08-02 21:41:23 +0530

    Add files via upload

---
+++ src/com/pCloudy/testNG/temp.java
@@ -0,0 +1,39 @@
+//package com.pCloudy.testNG;
+//
+//public class temp {
+//
+//
+//capabilities.setCapability("pCloudy_Username", "..........");
+//
+//capabilities.setCapability("pCloudy_ApiKey", ".............");
+//
+//capabilities.setCapability("pCloudy_DurationInMinutes", 10);
+//
+//capabilities.setCapability("newCommandTimeout", 600);
+//
+//capabilities.setCapability("launchTimeout", 90000);
+//
+//capabilities.setCapability("pCloudy_DeviceFullName", "APPLE_iPhone11Pro_iOS_17.5.1_45645");
+//
+//capabilities.setCapability("platformVersion", "17.5.1");
+//
+//capabilities.setCapability("platformName", "ios");
+//
+//capabilities.setCapability("acceptAlerts", true);
+//
+//capabilities.setCapability("automationName", "XCUITest");
+//
+//capabilities.setBrowserName("Safari");
+//
+//capabilities.setCapability("pCloudy_WildNet", "false");
+//
+//capabilities.setCapability("pCloudy_EnableVideo", "true");
+//
+//capabilities.setCapability("pCloudy_EnablePerformanceData", "true");
+//
+//capabilities.setCapability("pCloudy_EnableDeviceLogs", "true");
+//
+//capabilities.setCapability("appiumVersion", "1.22.0");
+//
+//    IOSDriver<WebElement> driver = new IOSDriver<WebElement>(new URL("https://lab.pcloudy.com/appiumcloud/wd/hub"), capabilities);
+//}
